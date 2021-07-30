f406ed8473d157ccecefc1ec3fa283aad5856f31
Destinovant <pcr.hillebrand1998@gmail.com>
2021-07-30 14:26:15 +0200

    oopsy, forgot the link: https://support-dev.discord.com/hc/en-us/articles/4404772028055-Message-Content-Access-Deprecation-for-Verified-Bots

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -5,7 +5,4 @@ module.exports = async (client) => {
   log('READY: Now receiving events and interactions\n', 'success')
   logger.info(`Logged in as ${client.user.tag}`)
   loadSlashCommands(client)
-  if (process.env.NODE_ENV !== 'production') {
-    // Only executes in non-production environments
-  }
 }
