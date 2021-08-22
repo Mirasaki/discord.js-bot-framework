92f5047eedbe76f3c27bb2e02663207406d2a3c4
daerunia <pcr.hillebrand1998@gmail.com>
2021-08-22 15:43:49 +0200

    include full and minimal command template

---
+++ src/commands/.minimalCommandTemplate.js
@@ -0,0 +1,17 @@
+exports.run = ({ client, interaction, guildSettings, args, emojis }) => {
+  //
+}
+
+exports.config = {
+  enabled: null,
+  required: null,
+  permLevel: ''
+}
+
+exports.slash = {
+  description: '',
+  enabled: true,
+  reload: false,
+  globalCommand: false,
+  options: [],
+}
