437dd1a4c23e84238ed4e7d099746380e1218e5d
daerunia <pcr.hillebrand1998@gmail.com>
2021-08-23 13:07:27 +0200

    add botinvite command, as part of the tutorial

---
+++ src/commands/System/botinvite.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+exports.run = ({ client, interaction, guildSettings, args, emojis }) => {
+  const botInviteLink = client.json.config.links.botInvite
+    .replace(/{{clientId}}/, `${client.user.id}`);
+
+  interaction.reply({
+    embeds: [
+      new MessageEmbed()
+        .setColor(client.json.colors.main)
+        .setDescription(
+          stripIndents`${emojis.response.success} ${interaction.member}, 
+            here you go: [Click to invite](${botInviteLink} "Invite Me!")
+          `
+        )
+    ]
+  });
+};
+
+exports.config = {
+  permLevel: 'User'
+};
+
+exports.slash = {
+  description: 'Get the link to add this bot to other servers.',
+};
