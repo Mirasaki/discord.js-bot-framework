c8d0cfbcb52860c400a7c9fcc7221d052389d6d9
daerunia <pcr.hillebrand1998@gmail.com>
2021-08-23 10:12:37 +0200

    remove some Intents

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ const nodePath = require('path');
     intents: [
       Intents.FLAGS.GUILDS,
       Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.DIRECT_MESSAGES,
-      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-      Intents.FLAGS.GUILD_VOICE_STATES,
-      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
       Intents.FLAGS.GUILD_MEMBERS
     ],
     fetchAllMembers: true
