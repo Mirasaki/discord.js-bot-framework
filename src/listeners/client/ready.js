2f3f46bb322823044101e8a7234e2188eeb75b12
daerunia <pcr.hillebrand1998@gmail.com>
2021-08-23 23:58:23 +0200

    remove ugly console output

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -2,7 +2,6 @@ const { loadSlashCommands } = require('../../handlers/commands');
 const { log } = require('../../handlers/logger');
 
 module.exports = async (client) => {
-  log('READY: Now receiving events and interactions', 'success');
-  log(`Logged in as ${client.user.tag}\n`, 'info');
+  log(`READY: Logged in as <${client.user.tag}>! Now receiving events and interactions!\n`, 'success');
   loadSlashCommands(client);
 };
