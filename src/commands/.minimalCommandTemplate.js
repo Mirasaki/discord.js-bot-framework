45772228f7fab1377e8cebfa8cff35beaf0ef0d2
daerunia <pcr.hillebrand1998@gmail.com>
2021-08-23 09:05:11 +0200

    remove more optional config options

--- src/commands/.minimalCommandTemplate.js
+++ src/commands/.minimalCommandTemplate.js
@@ -3,15 +3,9 @@ exports.run = ({ client, interaction, guildSettings, args, emojis }) => {
 }
 
 exports.config = {
-  enabled: null,
-  required: null,
   permLevel: ''
 }
 
 exports.slash = {
   description: '',
-  enabled: true,
-  reload: false,
-  globalCommand: false,
-  options: [],
 }
